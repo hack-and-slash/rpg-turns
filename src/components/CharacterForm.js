39a15ad68fce5158d14b8e195e6535d7a99c3953
Victor Miguez <victorcineis@gmail.com>
2018-10-20 12:22:02 -0300

    Fix code standards

--- src/components/CharacterForm.js
+++ src/components/CharacterForm.js
@@ -1,34 +1,39 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Formik } from 'formik';
 
-const CharacterForm = ({ handleSubmit }) => {
-  return (
-    <Formik
-      initialValues={{ name: '', initiative: '' }}
-      onSubmit={handleSubmit}
-      render={props => {
-        return (
-          <form onSubmit={props.handleSubmit}>
-            <input
-              type="text"
-              name="name"
-              placeholder="Name"
-              value={props.values.name}
-              onChange={props.handleChange}
-              autoFocus
-            />
-            <input
-              type="number"
-              name="initiative"
-              placeholder="Initiative roll"
-              value={props.values.initiative}
-              onChange={props.handleChange}
-            />
-          <button type="submit">add</button>
-        </form>
-        )}}
-      />
-  )
-}
+
+const CharacterForm = ({ handleSubmit }) => (
+  <Formik
+    initialValues={{ name: '', initiative: '' }}
+    onSubmit={handleSubmit}
+    render={props => (
+      <form onSubmit={props.handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={props.values.name}
+          onChange={props.handleChange}
+        />
+        <input
+          type="number"
+          name="initiative"
+          placeholder="Initiative roll"
+          value={props.values.initiative}
+          onChange={props.handleChange}
+        />
+        <button type="submit">add</button>
+      </form>
+    )}
+  />
+);
+
+CharacterForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  values: PropTypes.object.isRequired,
+};
+
 
 export default CharacterForm;
