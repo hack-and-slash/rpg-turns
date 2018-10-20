431314ff8df2d88208c8869442be8358326e7924
Henrique Degrecci <henrique.degrecci@upx.com>
2018-10-20 15:04:34 -0300

    Updates

---
+++ precache-manifest.9a21610271bf8040b098707c3a548eb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6d7d5894a64d06fb086",
+    "url": "/rpg-turns/static/js/runtime~main.c6d7d589.js"
+  },
+  {
+    "revision": "648c78a18475d5ffbe0f",
+    "url": "/rpg-turns/static/js/main.648c78a1.chunk.js"
+  },
+  {
+    "revision": "bd6089ad6af4c8a36140",
+    "url": "/rpg-turns/static/js/1.bd6089ad.chunk.js"
+  },
+  {
+    "revision": "648c78a18475d5ffbe0f",
+    "url": "/rpg-turns/static/css/main.cc0cb4b9.chunk.css"
+  },
+  {
+    "revision": "923bd054e004312e0aabe17197a5e119",
+    "url": "/rpg-turns/index.html"
+  }
+];
