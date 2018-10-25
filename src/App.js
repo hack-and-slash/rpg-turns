a59f1b7146141302ec0abdb649062c5578e68aff
BrunohSL <bruno.heinrich@superlogica.com>
2018-10-24 21:05:37 -0300

    review

--- src/App.js
+++ src/App.js
@@ -5,33 +5,7 @@ import InitiativeList from './components/InitiativeList';
 
 class App extends Component {
   state = {
-    characters: [
-      {
-        name: 'teste1',
-        initiative: 8,
-        hp: 12,
-      },
-      {
-        name: 'teste2',
-        initiative: 5,
-        hp: 0,
-      },
-      {
-        name: 'teste3',
-        initiative: 7,
-        hp: 0,
-      },
-      {
-        name: 'teste4',
-        initiative: 3,
-        hp: 25,
-      },
-      {
-        name: 'teste4',
-        initiative: 2,
-        hp: 0,
-      },
-    ],
+    characters: [],
     turn: 0,
   }
 
@@ -55,8 +29,7 @@ class App extends Component {
     const { characters, turn } = this.state;
     let nextTurn = characters.length - 1 > turn ? turn + 1 : 0;
     while (characters[nextTurn].hp <= 0) {
-
-      nextTurn += 1;
+      nextTurn++;
 
       if (nextTurn >= characters.length - 1) {
         nextTurn = 0;
